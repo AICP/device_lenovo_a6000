f2a7c88c01ed745728d8bc1c6565bff37c4ec43c
dev-harsh1998 <harshitjain6751@gmail.com>
2017-06-09 18:50:16 +0200

    a6000: optimise target flags
    Change-Id: I68110718ee4e0c0cbf1beaf5fb469b4f0c2067d0

--- board/kernel.mk
+++ board/kernel.mk
@@ -6,6 +6,8 @@ BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_KERNEL_TAGS_OFFSET := 0x00000100
 BOARD_RAMDISK_OFFSET := 0x01000000
+TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
 TARGET_KERNEL_SOURCE := kernel/lenovo/msm8916
 TARGET_KERNEL_CONFIG := lineageos-wt86518_defconfig
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.8/bin
